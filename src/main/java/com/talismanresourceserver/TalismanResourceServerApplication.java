dff61298e81c182ba247a92b4ca3a2f4134416c4
maciej <macieks53@gmail.com>
2023-06-17 22:15:58 +0200

    Changed generation.

--- src/main/java/com/talismanresourceserver/TalismanResourceServerApplication.java
+++ src/main/java/com/talismanresourceserver/TalismanResourceServerApplication.java
@@ -2,7 +2,6 @@ package com.talismanresourceserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
 public class TalismanResourceServerApplication {
